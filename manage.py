79a682e36f22028ef8bb760d0781e2b44cc3ab68
Helio <hsmptg@gmail.com>
2016-02-29 11:29:21 +0000

    clean code

--- manage.py
+++ manage.py
@@ -7,19 +7,6 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 
-# # Works fine!!!
-# @socketio.on('connect', namespace='/test')
-# def test_connect():
-#     print("Connected (msg from manage.py)")
-#     # Works fine!!!
-#     socketio.emit('my response', {'data': 'Connected', 'count': 33}, namespace='/test')
-#
-#
-# @socketio.on('my event', namespace='/test')
-# def handle_my_custom_event(json):
-#     print('received json: ' + str(json))
-
-
 @manager.command
 def run():
     socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False)
